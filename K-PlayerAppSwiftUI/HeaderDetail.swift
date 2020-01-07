9ba059f6be611ca10cbcc01887f8e560786cee16
Yumibyte <ashraigosa@gmail.com>
2020-01-06 20:40:38 -0800

    added PlaylistIcon as a rectangle

---
+++ K-PlayerAppSwiftUI/HeaderDetail.swift
@@ -0,0 +1,21 @@
+//
+//  HeaderDetail.swift
+//  K-PlayerAppSwiftUI
+//
+//  Created by Ashley Raigosa on 1/5/20.
+//  Copyright © 2020 Ashley Raigosa. All rights reserved.
+//
+
+import SwiftUI
+
+struct HeaderDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HeaderDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderDetail()
+    }
+}
