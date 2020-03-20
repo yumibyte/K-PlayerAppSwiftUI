c3484a03561337643df7eaa9ad75a8b9b959e31a
Yumibyte <ashraigosa@gmail.com>
2020-03-19 22:13:33 -0700

    delegate methods and protocols to handle sign in for google. Created in AppDelegate

--- K-PlayerAppSwiftUI/Controllers/AppDelegate.swift
+++ K-PlayerAppSwiftUI/Controllers/AppDelegate.swift
@@ -7,17 +7,70 @@
 //
 
 import UIKit
-//import Firebase
+import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
+    
+    //MARK: - Google Sign in Functionality
+    
+    // Configure shared instance and set sign-in delegate
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        func application(_ application: UIApplication,
+                         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+          // Initialize sign-in
+          GIDSignIn.sharedInstance().clientID = "YOUR_CLIENT_ID"
+            GIDSignIn.sharedInstance().delegate = self as? GIDSignInDelegate
 
-    // MARK: UISceneSession Lifecycle
+          return true
+        }
+    }
+    
+    
+    // Handles URL received at end of authentication
+    @available(iOS 9.0, *)
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+      return GIDSignIn.sharedInstance().handle(url)
+    }
+    
+    // Handle Sign in process
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+              withError error: Error!) {
+      if let error = error {
+        if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
+          print("The user has not signed in before or they have since signed out.")
+        } else {
+          print("\(error.localizedDescription)")
+        }
+        return
+      }
+        
+        
+      // Perform any operations on signed in user here.
+      let userId = user.userID                  // For client-side use only!
+      let idToken = user.authentication.idToken // Safe to send to the server
+      let fullName = user.profile.name
+      let givenName = user.profile.givenName
+      let familyName = user.profile.familyName
+      let email = user.profile.email
+    }
+    
+    
+    // If user is unable to connect...
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
+              withError error: Error!) {
+      // Perform any operations when the user disconnects from app here.
+      // ...
+    }
+
+
+
+    // MARK: - UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
