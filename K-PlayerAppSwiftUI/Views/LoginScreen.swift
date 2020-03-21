84645a36d019e6d8cebc55bfd5d733cf93e87677
Yumibyte <ashraigosa@gmail.com>
2020-03-21 11:27:44 -0700

    Revert "added reusable UIViewController for GoogleSignIn and modified all Doubles/Floats with CGFloat()"
    This reverts commit adc3b67e73ad45087f1ead7de24bfb432a61568f.

--- K-PlayerAppSwiftUI/Views/LoginScreen.swift
+++ K-PlayerAppSwiftUI/Views/LoginScreen.swift
@@ -7,7 +7,6 @@
 //
 
 import SwiftUI
-import GoogleSignIn
 
 struct LoginScreen: View {
     
@@ -16,35 +15,37 @@ struct LoginScreen: View {
     
     var body: some View {
     
+    //MARK: - Header
         
-    //MARK: - UITextFields and login button
+    //MARK: - UITextFields
         VStack() {
             
                 Text("Sign in to K-Player")
-//                    .font(.largeTitle)
-//                    .foregroundColor(.primary)
-//                    .padding(70)
+                    .font(.largeTitle)
+                    .foregroundColor(.primary)
+                    .padding(70)
+            
         
             VStack() {
                 TextField("Username", text: $username) .padding()
-                    .overlay(RoundedRectangle(cornerRadius: CGFloat(8)).stroke(Color.gray, lineWidth: CGFloat(1.0)))
+                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1.0))
                     .scaleEffect(0.85)
                 
                 SecureField("Password", text: $password)
-                    .padding() .overlay(RoundedRectangle(cornerRadius: CGFloat(8)).stroke(Color.gray, CGFloat(lineWidth: 1.0)))
+                    .padding() .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1.0))
                     .scaleEffect(0.85)
                 
                 //MARK: - Login Options
-                Button(action: SocialLogin.attemptLoginGoogle, label: {
-                    Text("Login in")
+                Button(action: {
+                    print("hi!")
                 }) {
                     
-                        .frame(width: 335, height: CGFloat(50))
+                    Text("Log in ")
+                        .frame(width: 335, height: 50)
                         
                         .background(Color.blue)
                         .foregroundColor(.white)
-                        
-                        .cornerRadius(CGFloat(15.0))
+                        .cornerRadius(15.0)
                         .padding(30)
                 }
             
@@ -58,27 +59,6 @@ struct LoginScreen: View {
     .offset(x: 0, y: -80)
     }
 }
-    
-    //MARK: - Sign in with social option
-struct SocialLogin: UIViewRepresentable {
-    
-    func makeUIView(context: UIViewRepresentableContext<SocialLogin>) -> UIView {
-        return UIView()
-        
-    }
-    
-    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<SocialLogin>) {
-    }
-    
-    func attemptLoginGoogle() {
-        GIDSignIn.sharedInstance()?.presentingViewController = UIApplication.shared.windows.last?.rootViewController
-        GIDSignIn.sharedInstance()?.signIn()
-    }
-    
-}
-   
-    
-
 
 struct LoginScreen_Previews: PreviewProvider {
     static var previews: some View {
