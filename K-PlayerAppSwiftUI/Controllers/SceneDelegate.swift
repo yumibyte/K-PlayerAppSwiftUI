1a7ea27e213bb5ee179257d7613e78617e97d3c9
Yumibyte <ashraigosa@gmail.com>
2020-03-30 23:00:54 -0700

    touchups in delegate

--- K-PlayerAppSwiftUI/Controllers/SceneDelegate.swift
+++ K-PlayerAppSwiftUI/Controllers/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        let contentView = WelcomeScreen()
+        let contentView = LoginScreen()
         
         // conditionally render authentication message if user isn't logged in
         window?.rootViewController = UIHostingController(
